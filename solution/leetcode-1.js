6473a0273dcad6b3b4227a7df29d1ca3d817f4de
JIE ZHANG <zhangjie9269@vip.qq.com>
2016-10-01 12:51:17 -0400

    Update leetcode-1.js

--- solution/leetcode-1.js
+++ solution/leetcode-1.js
@@ -1,1 +1,26 @@
+/*Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution.
+*/
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var len = nums.length;
+    var obj={};
+    var arr=[];
+    for(var i=0;i<len;i++){
+        obj[nums[i]]=i;
+    }
+    for(var j = 0;j<len;j++){
+        var temp = target-nums[j];
+        if(obj[temp]){
+            arr.push(j);
+            arr.push(obj[temp])
+            return arr;
+        }
+    }
+};
