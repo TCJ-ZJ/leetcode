b83fb300047d84b5ca978dbcb15ab82f51d64658
JIE ZHANG <zhangjie9269@vip.qq.com>
2016-10-02 14:12:09 -0400

    Create leetcode-9.js

---
+++ solution/leetcode-9.js
@@ -0,0 +1,22 @@
+/*Palindrome Number
+Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0) return false;
+    if(x===0) return true;
+    if(x>2147483647) return false;
+    var temp=x,
+    k=0;
+    while(temp>0){
+        k=k*10+temp%10;
+        temp=~~(temp/10);
+    }
+    if(k===x) return true;
+    return false;
+};
