59588a4f00a991397d10f7524a3261d483358ed8
JIE ZHANG <zhangjie9269@vip.qq.com>
2016-10-03 19:58:40 -0400

    Create leetcode-191-Number of 1 Bits.js

---
+++ solution/leetcode-191-Number of 1 Bits.js
@@ -0,0 +1,14 @@
+/*Number of 1 Bits
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    return n.toString(2).split('').reduce(function(pre,next){
+        return (pre-0)+(next-0);
+    })-0;
+};
