411a68e775092be1009b43b519ffd7b2a6a162ab
JIE ZHANG <zhangjie9269@vip.qq.com>
2016-10-03 21:28:29 -0400

    Create leetcode-70-Climbing Stairs.js

---
+++ solution/leetcode-70-Climbing Stairs.js
@@ -0,0 +1,19 @@
+/*Climbing Stairs
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    var arr=[];
+    arr[0]=1;
+    arr[1]=1;
+    arr[2]=2;
+    for(var i=3;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+};
